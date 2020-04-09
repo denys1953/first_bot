c20c47a20cdadd63bd1da401ca10c4de0b855e2d
denys1953 <den25640@gmail.com>
2020-04-09 10:10:01 +0300

    master

---
+++ index.js
@@ -0,0 +1,27 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '1209605305:AAGh7vRrHTwKoi_2aRf1PPsyBfHCs9XRBg4';
+
+var bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++) {
+    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
+    if (notes[i]['time'] === curDate) {
+      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+      notes.splice(i, 1);
+    }
+  }
+}, 1000);
