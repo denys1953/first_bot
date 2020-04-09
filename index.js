af6b741a43a5652dbe1227aa2680a1f9e83fa8b1
denys1953 <den25640@gmail.com>
2020-04-09 10:30:54 +0300

    master

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+  res.end('')
+})
+
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = '1209605305:AAGh7vRrHTwKoi_2aRf1PPsyBfHCs9XRBg4';
